0bf2eff9c9ac58b0f8b419df43f365758409215f
Colin de Groot <colindegroot2004@gmail.com>
2021-01-14 19:56:57 +0100

    engelse versie van de landingspage toegevoegd

---
+++ Landingspage/LandingspageEngels/js/ScriptSliderKlein.js
@@ -0,0 +1,152 @@
+let fotonummer, fotobreedte, aantalfotos, fotoslider;
+
+function setupFotoslider(){
+    // alert("lol")
+    fotonummer = 0;
+    fotobreedte = 400; // Pas dit aan naar de breedte van jouw foto's
+    fotoslider = document.getElementById("fotoslider");
+    let volgendeKnop = document.getElementById("volgende-btn");
+    let vorigeKnop = document.getElementById("vorige-btn");
+    aantalfotos = fotoslider.children.length;
+    console.log(aantalfotos);
+    volgendeKnop.addEventListener('click', volgendeFoto);
+    vorigeKnop.addEventListener('click', vorigeFoto);
+}
+
+function volgendeFoto(){
+    // alert('volgende geklikt');
+    // De variabele fotonummer met 1 ophogen (bij de eerste foto is fotonummer 0)
+    // Bij de tweede foto wordt deze dus 1, bij de derde foto 2 enzovoorts.
+    fotonummer += 1;
+
+    // Als fotonummer gelijk is aan het aantal foto's willen we weer terug naar de eerste foto
+    if (fotonummer === aantalfotos) {
+        fotonummer = 0;
+    }
+
+    // Dit is de berekening die ik hierboven heb uitgelegd
+    // Bereken aan de hand van het fotonummer hoe veel de slider naar links moet in de viewport
+    let afstandNaarLinks = -1 * (fotonummer * fotobreedte)
+
+    // Zet het style attribuut van de fotoslider (vergeet de px niet)
+    fotoslider.style.left = afstandNaarLinks + "px";
+}
+
+function vorigeFoto(){
+    // alert('volgende geklikt');
+    // De variabele fotonummer met 1 ophogen (bij de eerste foto is fotonummer 0)
+    // Bij de tweede foto wordt deze dus 1, bij de derde foto 2 enzovoorts.
+    fotonummer -= 1;
+
+    // Als fotonummer gelijk is aan het aantal foto's willen we weer terug naar de eerste foto
+    if (fotonummer < 0) {
+        fotonummer = aantalfotos - 1;
+    }
+
+    // Dit is de berekening die ik hierboven heb uitgelegd
+    // Bereken aan de hand van het fotonummer hoe veel de slider naar links moet in de viewport
+    let afstandNaarLinks = -1 * (fotonummer * fotobreedte)
+
+    // Zet het style attribuut van de fotoslider (vergeet de px niet)
+    fotoslider.style.left = afstandNaarLinks + "px";
+}
+
+window.addEventListener('DOMContentLoaded', setupFotoslider)
+
+//hieronder hover functie
+
+function setuphoverevent(){
+    // alert("lol");
+    let Foto1 = document.getElementById("foto1");
+    let Foto2 = document.getElementById("foto2");
+    let Foto3 = document.getElementById("foto3");
+    let Foto4 = document.getElementById("foto4");
+    
+    let Foto6 = document.getElementById("foto6");
+    
+
+    let Foto1Uit = document.getElementById("foto1");
+    let Foto2Uit = document.getElementById("foto2");
+    let Foto3Uit = document.getElementById("foto3");
+    let Foto4Uit = document.getElementById("foto4");
+    
+    let Foto6Uit = document.getElementById("foto6");
+    
+    
+    Foto1.addEventListener('mouseover', TextFoto1);
+    Foto2.addEventListener('mouseover', TextFoto2);
+    Foto3.addEventListener('mouseover', TextFoto3);
+    Foto4.addEventListener('mouseover', TextFoto4);
+    
+    Foto6.addEventListener('mouseover', TextFoto6);
+    
+    Foto1Uit.addEventListener('mouseout', TextFoto1Uit);
+    Foto2Uit.addEventListener('mouseout', TextFoto2Uit);
+    Foto3Uit.addEventListener('mouseout', TextFoto3Uit);
+    Foto4Uit.addEventListener('mouseout', TextFoto4Uit);
+    
+    Foto6Uit.addEventListener('mouseout', TextFoto6Uit);
+    
+
+}
+
+function TextFoto1(){
+    document.getElementById("text-fotoslider").innerHTML = "Kunstwerk: 'Het laatste avondmaal'";
+    document.getElementById("text-fotoslider").style.color = "black";
+}
+
+function TextFoto2(){
+    document.getElementById("text-fotoslider").innerHTML = "Kunstwerk 'Bon Appétit!'";
+    document.getElementById("text-fotoslider").style.color = "black";
+}
+
+function TextFoto3(){
+    document.getElementById("text-fotoslider").innerHTML = "Kunstwerk 'De Koffiepot'";
+    document.getElementById("text-fotoslider").style.color = "black";
+}
+
+function TextFoto4(){
+    document.getElementById("text-fotoslider").innerHTML = "Kunstwerk 'Woman with shopping bag'";
+    document.getElementById("text-fotoslider").style.color = "black";
+}
+
+
+
+function TextFoto6(){
+    document.getElementById("text-fotoslider").innerHTML = "Kunstwerk 'Food Chain Project'";
+    document.getElementById("text-fotoslider").style.color = "black";
+}
+
+
+// vanaf hier de functies uit
+
+function TextFoto1Uit(){
+    document.getElementById("text-fotoslider").innerHTML = ".";
+    document.getElementById("text-fotoslider").style.color = "white";
+}
+
+function TextFoto2Uit(){
+    document.getElementById("text-fotoslider").innerHTML = ".";
+    document.getElementById("text-fotoslider").style.color = "white";
+}
+
+function TextFoto3Uit(){
+    document.getElementById("text-fotoslider").innerHTML = ".";
+    document.getElementById("text-fotoslider").style.color = "white";
+}
+
+function TextFoto4Uit(){
+    document.getElementById("text-fotoslider").innerHTML = ".";
+    document.getElementById("text-fotoslider").style.color = "white";
+}
+
+
+
+function TextFoto6Uit(){
+    document.getElementById("text-fotoslider").innerHTML = ".";
+    document.getElementById("text-fotoslider").style.color = "white";
+}
+
+
+
+window.addEventListener('DOMContentLoaded', setuphoverevent)
---
+++ Landingspage/LandingspageEngels/js/modal.js
@@ -0,0 +1,154 @@
+// vanaf hier komt de code voor de code voor de modal
+// de knoppen 
+var knop1 = document.getElementById("knop1")
+
+var knop2 = document.getElementById("knop2")
+
+var knop3 = document.getElementById("knop3")
+
+// de modal
+var modal = document.getElementById("myModal");
+// var modal1 = document.getElementById()
+var modal2 = document.getElementById("myModal2")
+// de button die de modal opent
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// de modal openen wanneer er op de knop gebrukt word 
+btn.onclick = function openModal() {
+    modal.style.display = "block";
+    
+}
+
+// sluit de modal wanneer de gebruiker op het kruisje(x)klikt
+span.onclick = function() {
+    modal.style.display = "none";
+    modal2.style.display = "none";
+}
+
+// sluit de modal wanneer de gebruiker er buiten klikt
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        modal2.style.display = "none";
+    }
+}
+
+
+
+
+// vanaf hier komt de code voor de quiz!!!!!!!!
+
+
+
+var c = 0;
+var foto = document.getElementById("fotoEindschermKlein");
+
+function SpeelQuiz(){
+    
+    // vraag1
+    if (document.getElementById("knop1vraag1").checked){
+        c++;
+    } else if (document.getElementById("knop2vraag1").checked){
+        c++;
+        c++;
+    } else if (document.getElementById("knop3vraag1").checked){
+        c++;
+        c++;
+        c++;
+    }
+
+    // vraag2
+    if (document.getElementById("knop1vraag2").checked){
+        c++;
+        c++;
+    } else if (document.getElementById("knop2vraag2").checked){
+        c++;
+        
+    } else if (document.getElementById("knop3vraag2").checked){
+        c++;
+        c++;
+        c++;
+    }
+
+    // vraag3
+    if (document.getElementById("knop1vraag3").checked){
+        c++;
+        c++;
+    } else if (document.getElementById("knop2vraag3").checked){
+        c++;
+        c++;
+        c++;
+    } else if (document.getElementById("knop3vraag3").checked){
+        c++;
+        
+    }
+
+    // vraag4
+    if (document.getElementById("knop1vraag4").checked){
+        c++;
+        c++;
+        c++;
+    } else if (document.getElementById("knop2vraag4").checked){
+        c++;
+        c++;
+    } else if (document.getElementById("knop3vraag4").checked){
+        c++;
+        
+    }
+
+    // vraag5
+    if (document.getElementById("knop1vraag5").checked){
+        c++;
+        c++;
+    } else if (document.getElementById("knop2vraag5").checked){
+        c++;
+        c++;
+        c++;
+    } else if (document.getElementById("knop3vraag5").checked){
+        c++;
+        
+    }
+
+    // vraag6
+    if (document.getElementById("knop1vraag6").checked){
+        c++;
+    } else if (document.getElementById("knop2vraag6").checked){
+        c++;
+        c++;
+        c++;
+    } else if (document.getElementById("knop3vraag6").checked){
+        c++;
+        c++;
+        
+    }
+    console.log(c);
+    eindscherm();
+}
+
+function eindscherm(){
+    if (c < 7){
+        foto.src = "kunstwerkenfotos/kunstwerk1.jpg";
+        document.getElementById("naamKunst").innerHTML = "Jij bent het kunstwerk: 'Het laatste avondmaal!'";
+    }else if (c < 10){
+        foto.src = "kunstwerkenfotos/kunstwerk2.jpg";
+        document.getElementById("naamKunst").innerHTML = "Jij bent het kunstwerk: 'Bon Appétit!'";
+    }else if (c < 13){
+        foto.src = "kunstwerkenfotos/kunstwerk3.jpg";
+        document.getElementById("naamKunst").innerHTML = "Jij bent het kunstwerk: 'De Koffiepot'";
+    }else if (c < 16){
+        foto.src = "kunstwerkenfotos/kunstwerk4.JPG";
+        document.getElementById("naamKunst").innerHTML = "Jij bent het kunstwerk: 'Woman with shopping bag'";
+    }else if (c < 19){
+        foto.src = "kunstwerkenfotos/kunstwerk5.jpg";
+        document.getElementById("naamKunst").innerHTML = "Jij bent het kunstwerk: 'Food Chain Project'";
+    }
+    modal.style.display="none";
+    modal2.style.display="block";
+    
+}
+function Home(){
+    location.reload();
+}
